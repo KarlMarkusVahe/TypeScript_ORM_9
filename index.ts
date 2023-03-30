08dde9802001cfe58feac590d46febf9c3d4cbd5
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-03-30 13:17:47 +0300

    Finshed project

---
+++ index.ts
@@ -0,0 +1,35 @@
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser"
+
+import articleController from "./controllers/articleController"
+import commentController from "./controllers/commentController";
+import authorController from "./controllers/authorController"
+
+mongoose.connect("mongodb+srv://karlmarkusvaher:awktzLXcx9emq9fE@cluster0.tjkufer.mongodb.net/test");
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app: Express = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.use('/', articleController);
+app.use('/', commentController);
+app.use('/', authorController);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(3000,() => {
+    console.log(`[server]: Server is running at http://localhost:3000`);
+});
